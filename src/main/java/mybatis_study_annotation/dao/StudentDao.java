d9b29efb932943f7c0cf190b0ec7a441f00e6964
teacher <net94.teacher@gmail.com>
2020-02-01 14:13:40 +0900

    aa

--- src/main/java/mybatis_study_annotation/dao/StudentDao.java
+++ src/main/java/mybatis_study_annotation/dao/StudentDao.java
@@ -7,9 +7,10 @@ import org.apache.ibatis.annotations.Select;
 import mybatis_study_annotation.dto.Student;
 
 public interface StudentDao {
-	@Select("select STUD_ID, NAME, EMAIL, PHONE, DOB from students where stud_id=#{studId}")
-	Student selectStudentByNo(Student student);
-
-	@Select("select STUD_ID, NAME, EMAIL, PHONE, DOB from students")
-	List<Student> selectStudentByAll();
+//	@Select("select STUD_ID, NAME, EMAIL, PHONE, DOB from students where stud_id=#{studId}")
+//	Student selectStudentByNo(Student student);
+//
+//	@Select("select STUD_ID, NAME, EMAIL, PHONE, DOB from students")
+//	List<Student> selectStudentByAll();
+	
 }
--- src/main/java/mybatis_study_annotation/daoimpl/StudentDaoImpl.java
+++ src/main/java/mybatis_study_annotation/daoimpl/StudentDaoImpl.java
@@ -10,6 +10,7 @@ import mybatis_study_annotation.jdbc.MyBatisSqlSessionFactory;
 
 public class StudentDaoImpl implements StudentDao {
 	private String namespace = "mybatis_study_annotation.dao.StudentDao";
+	
 	private static final StudentDaoImpl instance = new StudentDaoImpl();
 	
 	private StudentDaoImpl() {
@@ -20,19 +21,19 @@ public class StudentDaoImpl implements StudentDao {
 		return instance;
 	}
 
-	@Override
-	public Student selectStudentByNo(Student student) {
-		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
-			return sqlSession.selectOne(namespace + ".selectStudentByNo", student);
-		}
-
-	}
-
-	@Override
-	public List<Student> selectStudentByAll() {
-		try(SqlSession sqlSession = MyBatisSqlSessionFactory.openSession()){
-			return sqlSession.selectList(namespace + ".selectStudentByAll");
-		}
-	}
+//	@Override
+//	public Student selectStudentByNo(Student student) {
+//		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+//			return sqlSession.selectOne(namespace + ".selectStudentByNo", student);
+//		}
+//
+//	}
+//
+//	@Override
+//	public List<Student> selectStudentByAll() {
+//		try(SqlSession sqlSession = MyBatisSqlSessionFactory.openSession()){
+//			return sqlSession.selectList(namespace + ".selectStudentByAll");
+//		}
+//	}
 
 }
