96e3c9b5aa141ccaf26dd5dabf81202247d51a91
teacher <net94.teacher@gmail.com>
2020-02-02 13:42:49 +0900

    course insert, delete, select 구현

---
+++ src/main/java/mybatis_study_annotation/transaction/CourseTutorTransaction.java
@@ -0,0 +1,32 @@
+package mybatis_study_annotation.transaction;
+
+import org.apache.ibatis.session.SqlSession;
+
+import mybatis_study_annotation.dto.Course;
+import mybatis_study_annotation.dto.Tutor;
+import mybatis_study_annotation.jdbc.MyBatisSqlSessionFactory;
+
+public class CourseTutorTransaction {
+	private String namespace_tutor = "mybatis_study_annotation.dao.TutorDao";
+	private String namespace_course = "mybatis_study_annotation.dao.CourseDao";
+
+	public void joinNewTutorAndCourse(Tutor tutor, Course course) {
+        SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();
+        int res = 0;
+        try {
+            res += sqlSession.insert(namespace_tutor + ".insertTutor", tutor);
+            res += sqlSession.insert(namespace_course + ".insertCourse", course);
+            if (res == 2)
+                sqlSession.commit();
+            else
+                throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+            throw new RuntimeException(e.getCause());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+}
