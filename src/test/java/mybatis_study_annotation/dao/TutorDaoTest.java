3d3fdbfeb8ba133a17081dd6a11f8d9ec88dd20a
teacher <net94.teacher@gmail.com>
2020-02-02 11:44:39 +0900

    매퍼 애너테이션 - 동적 SQL(@SelectProvider)

--- src/test/java/mybatis_study_annotation/dao/TutorDaoTest.java
+++ src/test/java/mybatis_study_annotation/dao/TutorDaoTest.java
@@ -1,17 +1,24 @@
 package mybatis_study_annotation.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import mybatis_study_annotation.AbstractTest;
 import mybatis_study_annotation.daoimpl.TutorDaoImpl;
 import mybatis_study_annotation.dto.Tutor;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TutorDaoTest extends AbstractTest {
 	private TutorDao dao = TutorDaoImpl.getInstance();
 
 	@Test
-	public void testSelectTutorById() {
+	public void test01SelectTutorById() {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 
 		Tutor selTutor = dao.selectTutorById(1);
@@ -21,7 +28,7 @@ public class TutorDaoTest extends AbstractTest {
 	}
 
 	@Test
-	public void testSelectTutorByTutorId() {
+	public void test02SelectTutorByTutorId() {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Tutor tutor = new Tutor();
 		tutor.setTutorId(1);
@@ -30,4 +37,46 @@ public class TutorDaoTest extends AbstractTest {
 		Assert.assertNotNull(selTutor);
 		log.trace(selTutor.toString());
 	}
+	
+	@Test
+	public void test03SelectAllTutorsProv() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		List<Tutor> lists = dao.selectAllTutorsProv();
+		Assert.assertNotNull(lists);
+		for(Tutor t : lists) log.trace(t.toString());
+	}
+
+	@Test
+	public void test04SelectTutorProv() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Map<String, Object> map = null;
+		List<Tutor> lists = dao.selectTutorProv(map);
+		for(Tutor t : lists) log.trace(t.toString());
+		
+		map = new HashMap<>();
+		map.put("tutorId", 1);
+
+		lists = dao.selectTutorProv(map);
+		Assert.assertNotNull(lists);
+		for(Tutor t : lists) log.trace(t.toString());
+	}
+
+	@Test
+	public void test05SelectTutorByJoinProv() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Map<String, Object> map = null;
+		List<Tutor> lists = dao.selectTutorByJoinProv(map);
+		for(Tutor t : lists) log.trace(t.toString());
+		
+		map = new HashMap<>();
+		map.put("tutorId", 1);
+		lists = dao.selectTutorByJoinProv(map);
+		for(Tutor t : lists) log.trace(t.toString());
+		
+		map.put("addrId", 1);
+		lists = dao.selectTutorByJoinProv(map);
+		Assert.assertNotNull(lists);
+		for(Tutor t : lists) log.trace(t.toString());
+	}   
+
 }
